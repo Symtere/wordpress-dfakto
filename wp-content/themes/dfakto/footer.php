76f6a5b6b8f3ce2bee424acdaf1f3f27346cf355
gsupiot-webexpr <gsupiot@webexpr.fr>
2022-10-09 16:13:17 +0200

    footer

--- wp-content/themes/dfakto/footer.php
+++ wp-content/themes/dfakto/footer.php
@@ -3,24 +3,37 @@
         <footer id="footer" class="footer">
             <div class="container">
                 <div class="sup-footer d-flex justify-content-md-between justify-content-center">
-                    <div class="ft-brand text-center">
-                        <?php echo function_exists('get_acf_logo_footer') ? '<div class="ft-brand-img">' . get_acf_logo_footer() . '</div>' : ''; ?>
-                        <?php echo function_exists('get_brand_informations') ? get_brand_informations() : ''; ?>
+                    <div class="ft-menu">
+                        <?php //echo function_exists('get_acf_logo_footer') ? '<div class="ft-brand-img">' . get_acf_logo_footer() . '</div>' : '';
+                        ?>
+                        <?php //echo function_exists('get_brand_informations') ? get_brand_informations() : '';
+                        ?>
+                        <?php echo function_exists('footer_nav') ? footer_nav() : ''; ?>
                     </div>
-                    <?php echo function_exists('footer_nav') ? footer_nav() : ''; ?>
+
+                    <?php $adresses = get_field_option('adresses') ? get_field_option('adresses') : '';
+                    foreach ($adresses as $key => $address) { ?>
+                        <div class="ft-col-adress">
+                            <div class="menu-nav-title"><?php echo $address['title'] ?></div>
+                            <div class="ft-address"><?php echo $address['address'] ?></div>
+                            <div class="ft-addr-phone"><a href="<?php echo 'tel:' . $address['phone']; ?>"><?php echo $address['phone'] ?></a></div>
+                        </div>
+                    <?php } ?>
+
+                    <div class="ft-rsx-scx">
+                        <?php echo function_exists('get_social_items') ? sprintf('<div class="menu-nav-title">Suivez nous sur les réseau sociaux</div>%s', get_social_items()) : ''; ?>
+                    </div>
                 </div>
             </div>
             <div class="sub-footer">
                 <div class="container">
                     <div class="d-md-flex align-items-center justify-content-between">
-                        <div class="sf-links order-md-2 mb-3 mb-md-0">
-                            <?php echo function_exists('rgpd_nav') ? rgpd_nav() : ''; ?>
+                        <div class="sf-links mb-3 mb-md-0">
+                            <?php echo esc_attr(get_bloginfo('name')); ?> <?php echo date('Y'); ?><?php echo function_exists('rgpd_nav') ? rgpd_nav() : ''; ?>
                         </div>
                         <div class="d-flex flex-wrap ft-copyright align-items-center">
                             <div class="pe-2">
-                                <?php echo __( "Copyright", 'custom' ); ?> <?php echo date( 'Y' ); ?>
-                                © &nbsp;<span class="text-uppercase"><?php echo esc_attr(get_bloginfo( 'name' )); ?></span>
-                                <?php echo __( "Réalisation et hébergement par", 'custom' ); ?>
+                                Mis à flot par <a target="_blank" href="https://www.pilot-in.com/">Pilot’in</a>
                             </div>
                         </div>
                     </div>
@@ -28,11 +41,11 @@
             </div>
         </footer>
 
-        <?php get_template_part( 'template-parts/aside-menu' ); ?>
+        <?php get_template_part('template-parts/aside-menu'); ?>
 
         <?php wp_footer(); ?>
-    </div>
+        </div>
 
-</body>
-</html>
+        </body>
 
+        </html>
